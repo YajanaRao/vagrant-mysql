2a6771cc4e4787a3d3ea85343b247cb22f9d5d66
Yajana N Rao <yajananrao@gmail.com>
2021-06-02 09:50:38 +0530

    Update DEPARTMENT.sql

--- tutorial/DEPARTMENT.sql
+++ tutorial/DEPARTMENT.sql
@@ -4,9 +4,16 @@ insert into dept values(1,'E&C','hubli');
 
 insert into dept values(2,'Mechanical','hubli');
 
-insert into dept values(3,'Computer Science','hubli');
+insert into dept values(3,'Computer Science','Chicago');
 
+insert into dept values(20,'research','Chicago');
 
 select * from dept;
 
 select * from dept where dept_no = 3;
+
+select dept_name from dept;
+
+select * from dept where loc = 'Chicago';
+
+select loc from dept where  dept_name= 'research' and dept_no=20;
